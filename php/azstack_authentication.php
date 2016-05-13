05ee82356102524230cb653e97a9eb7f25c8b267
huydaungoc <huydaungoc>
2016-05-13 11:16:46 +0700

    accept application/json HTTP POST

--- php/azstack_authentication.php
+++ php/azstack_authentication.php
@@ -1,6 +1,8 @@
 <?php
 
-//App: Test and document
+//App: Test and document, appID: 26870527d2ac628002dda81be54217cf
+//Important: change $secretCode, $privateKey value (go to: https://developers.azstack.co, menu: Keys)
+
 $secretCode = 'f0d1ba666tttbed3';
 $privateKey = 'MIIEwAIBADANBgkqhkiG9w0BAQEFAASCBKowggSmAgEAAoIBAQCr2zjTtCQyJmRc
 XSO4IaO2LqJZQPOoSY/5Suzf6DbM5/6tsgBlVki1LlrgFmW/cO7HdeeaGvPbjQ5k
@@ -18,10 +20,25 @@ AgfEYVbTS3YRC30gzpo9QsklqWS/btQi0JsdFlOgHau8XrPpoIDuWlQ6jwtbK/aY
 4APC746ry95yz3N6oH7rND61ufaApReGlSV7zCPC5N97ymJEt9p0G0Xil0S0Ms8/
 LABVOpAYoJBwxaaNnHlyPBpuYtg=';
 
+
+
+
+
+
 set_include_path(get_include_path() . PATH_SEPARATOR . './phpseclib1.0.0');
 include('Crypt/RSA.php');
 
-$token = @$_POST['token'];
+$token = @$_POST['token'];//application/x-www-form-urlencoded
+if(!$token){
+	$post_json = file_get_contents('php://input');//application/json
+	if ($post_json) {
+		$post_json = json_decode($post_json);
+		$token = $post_json->token;
+	}
+}
+
+error_log('token: ' . $token);
+
 $result = 1;
 if ($token) {
 	$rsa = new Crypt_RSA();
